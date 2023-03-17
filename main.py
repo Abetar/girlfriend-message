26dff241c4bfb2c62b68ce0f4e13fbd34f693c7b
Abraham Alexis Gomez Arrazate <47872184+Abetar@users.noreply.github.com>
2023-03-17 01:10:01 -0600

    Adding a final result

---
+++ main.py
@@ -0,0 +1,19 @@
+
+status = True
+
+while status:
+
+    print("Cual es la fecha especial?")
+    response = input()
+
+
+    if(str(response) != "18082021"):
+        print("Fecha equivocada, pero aun asi te amo :)")
+        
+    else:
+        print("Eres la mejor novia que existe, a pesar e las peleas mi amor por ti siempre va a ser mas grande y se que juntos saldremos adelante. Te amo <3")
+        status = False
+        
+
+
+
